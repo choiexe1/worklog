1c3a6c57223e1de4ab908dc0bccfa953900a718c
choiexe1 <choiexe1@gmail.com>
2024-06-24 15:35:58 +0900

    FastApi modelling test

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Log(BaseModel):
+    name: str
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(req: Request):
+    return {"api": "0.0.1"}
+
+@app.get("/{name}")
+async def test(name: str):
+    return Log(name=name)
